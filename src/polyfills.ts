821ac7b78b59b3ca60bf3a5c436c16287a347d44
lukaszczernal <lukaszczernal@wp.pl>
2016-11-27 19:12:22 +0100

    Electron is here

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -16,4 +16,7 @@ import 'core-js/es6/set';
 import 'core-js/es6/reflect';
 
 import 'core-js/es7/reflect';
+
+// modified line
 import 'zone.js/dist/zone';
+// import 'zone.js/dist/zone-node'; // TODO check i we need to switch to node
